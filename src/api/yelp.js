02de9338ec071e4e69a9816aeaf73d218183896a
alijb <alijb@post.bgu.ac.il>
2020-09-24 20:48:36 +0300

    Successfully Made an api request and get a response from yelp

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,7 +1,9 @@
-const { default: Axios } = require("axios");
-
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "GET https://api.yelp.com/v3/businesses",
+  baseURL: "https://api.yelp.com/v3/businesses",
+  headers: {
+    Authorization:
+      "Bearer OqN0w5ndPHfGzudh9rcuBQ1TVTs3mG8FkfRn7yHV4gtCndWaKgWUt8LjGvchbwlE4vILXaaMnpQP-SIW4-gyO7I7LYsD9yp0LcBcd1qP8SXwRgZRW5TJxSMGoaZsX3Yx",
+  },
 });
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,20 +1,29 @@
 import React, { Component, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import yelp from "../api/yelp";
 import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get("/search", {
+      params: {
+        limit: 50,
+        term,
+        location: "san jose",
+      },
+    });
+    setResults(response.data.businesses);
+  };
 
   return (
     <View>
-      <SearchBar
-        term={term}
-        onTermSubmit={console.log("term submitted")}
-        onTermChange={(newTerm) => {
-          setTerm(newTerm);
-        }}
-      />
-      <Text style={styles.textSyle}>{term}</Text>
+      <SearchBar term={term} onTermSubmit={searchApi} onTermChange={setTerm} />
+      <Text style={styles.textSyle}>
+        We have found {results.length} results
+      </Text>
     </View>
   );
 };
