0afd3d673548011d3bf01a5c6f7fef7a79d67159
alijb <alijb@post.bgu.ac.il>
2020-09-24 21:09:23 +0300

    using useEffect for first rendering data loading

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import yelp from "../api/yelp";
 import SearchBar from "../components/SearchBar";
@@ -6,24 +6,39 @@ import SearchBar from "../components/SearchBar";
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const searchApi = async () => {
-    const response = await yelp.get("/search", {
-      params: {
-        limit: 50,
-        term,
-        location: "san jose",
-      },
-    });
-    setResults(response.data.businesses);
+  const searchApi = async (searchTerm) => {
+    console.log("hi");
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: "san jose",
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage("Something went wrong");
+    }
   };
+
+  useEffect(() => {
+    searchApi("pasta");
+  }, []);
 
   return (
     <View>
-      <SearchBar term={term} onTermSubmit={searchApi} onTermChange={setTerm} />
+      <SearchBar
+        term={term}
+        onTermSubmit={() => searchApi(term)}
+        onTermChange={setTerm}
+      />
       <Text style={styles.textSyle}>
         We have found {results.length} results
       </Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
     </View>
   );
 };
