53616cde6ffed02b802bad2e1c644c2862f63a83
alijb <alijb@post.bgu.ac.il>
2020-09-24 22:46:54 +0300

    Navigation.getParam Important to reme,ber

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet, TextInput } from "react-native";
 
-const ResultsShowScreen = ({ term, onTermChange, onTermSubmit }) => {
+const ResultsShowScreen = ({ navigation }) => {
+  navigation.getParam("id");
   return (
     <View style={styles.view}>
       <Text>Results Show</Text>
