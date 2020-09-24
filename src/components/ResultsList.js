3c94a9923c7a0f3bba080abc440e149e21ed8418
alijb <alijb@post.bgu.ac.il>
2020-09-24 23:07:11 +0300

    Finished !!!! :))) Styling maybe applied later

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -10,6 +10,9 @@ import { withNavigation } from "react-navigation";
 import ResultsDetail from "./ResultsDetail";
 
 const ResultsList = ({ title, results, navigation }) => {
+  if (!results.length) {
+    return null;
+  }
   return (
     <View>
       <Text style={styles.titleStyle}>{title}</Text>
