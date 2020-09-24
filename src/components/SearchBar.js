92b93c5c91b94c3c4f47f65946ded86f892a8560
alijb <alijb@post.bgu.ac.il>
2020-09-24 18:08:54 +0300

    finished styling search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,20 +4,22 @@ import { Feather } from "@expo/vector-icons";
 
 const SearchBar = () => {
   return (
-    <View style={styles.backgroud}>
+    <View style={styles.backgroudStyle}>
       <Feather name="search" size={30} color="black" />
-      <Text>Search Bar</Text>
+      <TextInput style={styles.inputStyle} placeholder="Search" />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  backgroud: {
+  backgroudStyle: {
     backgroundColor: "#F0EEEE",
     height: 50,
     borderRadius: 5,
     marginHorizontal: 15,
+    flexDirection: "row",
   },
+  inputStyle: { borderColor: "black", borderWidth: 1, flex: 1 },
 });
 
 export default SearchBar;
